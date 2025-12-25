efb112f8ae119c22a77d07be6919c6bc903f34fc
Tom <tom@contextfree.info>
2025-12-25 07:20:25 -0700

    Maybe finish cpp

--- capture.cpp
+++ capture.cpp
@@ -1,16 +1,45 @@
-#include <iostream>
 #include <functional>
+#include <iostream>
+#include <vector>
 
 void repeat(int times, std::function<void(int)> act) {
-    for (int i = 0; i < times; i += 1) {
+    for (int i = 0; i < times; ++i) {
         act(i);
     }
 }
 
+struct Hub {
+    void on_action(std::function<void()> handler) {
+        handlers.push_back(handler);
+    }
+
+    void action_performed() {
+        for (auto& handler: handlers) {
+            handler();
+        }
+    }
+
+private:
+    std::vector<std::function<void()>> handlers;
+};
+
+void init_hub(Hub& hub) {
+    for (int i = 0; i < 3; ++i) {
+        hub.on_action([i](){
+            std::cout << "i: " << i << "\n";
+        });
+    }
+}
+
 int main() {
+    // Sum
     int sum = 0;
     repeat(3, [&sum](int i) {
         sum += i;
     });
     std::cout << "sum: " << sum << "\n";
+    // Gather
+    Hub hub;
+    init_hub(hub);
+    hub.action_performed();
 }
