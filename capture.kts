0a15f4f99b55df5f2777b0e01141fb8183996908
Tom <tom@contextfree.info>
2025-12-26 07:04:41 -0700

    Add kotlin

---
+++ capture.kts
@@ -0,0 +1,43 @@
+fun main() {
+    // Repeat
+    var sum = 0
+    repeat(3) { i ->
+        sum += i
+    }
+    println("sum: $sum")
+
+    // Gather
+    val hub = Hub()
+    initHub(hub)
+    hub.actionPerformed()
+}
+
+fun repeat(times: Int, act: (Int) -> Unit) {
+    for (i in 0 until times) {
+        act(i)
+    }
+}
+
+fun initHub(hub: Hub) {
+    val items = mutableListOf<Int>()
+    for (i in 0 until 3) {
+        hub.onAction {
+            items.add(i)
+            println("items: $items")
+        }
+    }
+}
+
+class Hub {
+    private val handlers = mutableListOf<() -> Unit>()
+
+    fun onAction(handler: () -> Unit) {
+        handlers.add(handler)
+    }
+
+    fun actionPerformed() {
+        handlers.forEach { it() }
+    }
+}
+
+main()
